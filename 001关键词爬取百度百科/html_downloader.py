a315363065ae5ba6b025204e308ca05611c09b10
Kimisme <jinxin_x@126.com>
2017-02-28 15:47:42 +0800

    add baidu baike spider

---
+++ 001关键词爬取百度百科/html_downloader.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8-*-
+# module html_downloader
+import urllib2
+class HtmlDownloader:
+    def download(self,url):
+        if url is None:
+            return None
+
+        response = urllib2.urlopen(url)
+
+        if response.getcode()!=200:
+            return None
+        return response.read()
